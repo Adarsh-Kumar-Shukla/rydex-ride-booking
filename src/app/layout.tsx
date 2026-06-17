7b5a0f05b7bd0b4a84c50a5b34f2a94f39d09428
Adarsh-Kumar-Shukla <adarshkshuklasrw@gmail.com>
2026-06-17 10:55:08 +0530

    implemented redux toolkit for state management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/lib/Providers";
+import ReduxProvider from "@/redux/ReduxProvider";
+import Inituser from "@/Inituser";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +32,10 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <Provider>
-          {children}
+          <ReduxProvider>
+            <Inituser/>
+            {children}
+          </ReduxProvider>
         </Provider>
       </body>
     </html>
