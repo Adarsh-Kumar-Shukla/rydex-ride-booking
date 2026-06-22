9a384ef01945ad2b1d064802f2e0bed7923f794a
Adarsh-Kumar-Shukla <adarshkshuklasrw@gmail.com>
2026-06-22 22:48:26 +0530

    created partnerBank model file and partner document model file

---
+++ src/app/models/partnerBank.model.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+interface IPartnerBank {
+  owner: mongoose.Types.ObjectId;
+  accountHolder: string;
+  accountNumber: string;
+  ifsc: string;
+  upi: string;
+  status: "not_added" | "added" | "verified";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const partnerBankSchema = new mongoose.Schema<IPartnerBank>(
+  {
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    accountHolder:{
+      type:String,
+      required:true
+    },
+    accountNumber: {
+      type:String,
+      required:true,
+      unique:true
+    },
+    ifsc: {
+      type:String,
+      required:true,
+      uppercase:true
+    },
+    upi: {
+      type:String,
+      required:true
+    },
+    status: {
+      type: String,
+      emun: ["not_added", "added", "verified"],
+      default: "not_added",
+    },
+  },
+  { timestamps: true },
+);
+
+const PartnersBank =
+  mongoose.models.Vehicle || mongoose.model("PartnersBank", partnerBankSchema);
+export default PartnersBank;
---
+++ src/app/models/partnerDocs.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface IPartnerDocs {
+  owner: mongoose.Types.ObjectId;
+  aaddharUrl: string;
+  licenseUrl: string;
+  rcUrl: string;
+  status: "approved" | "pending" | "rejected";
+  rejectionReason?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const partnerDocsSchema = new mongoose.Schema<IPartnerDocs>(
+  {
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      emun: ["approved", "pending", "rejected"],
+      default: "pending",
+    },
+    rejectionReason: String,
+    aaddharUrl: String,
+    licenseUrl: String,
+    rcUrl: String,
+  },
+  { timestamps: true },
+);
+
+const PartnersDocs =
+  mongoose.models.Vehicle || mongoose.model("PartnersDocs", partnerDocsSchema);
+export default PartnersDocs;
